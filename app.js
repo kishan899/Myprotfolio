52bb25ea05c228505d2f8497e19e7d895681e9ba
It's Kishan <70309463+kishan899@users.noreply.github.com>
2023-09-15 21:25:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 })
 
 app.listen(3000)
