9756a3dea4b8368bf01b7471276e160e1a9c9e9e
It's Kishan <70309463+kishan899@users.noreply.github.com>
2023-09-16 11:32:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const { Client, MusicClient } = require("youtubei");
+const youtube = new Client();
 const app = express()
-
+app.get('/api/bot-info', async (req, res) => {
+  
+    const videos =  youtube.search("Hanuman chalisha", {
+        type: "video", // video | playlist | channel | all
+    });
+videos.then((respone) => {
+ console.log(respone.items)
+  res.send(respone)
+}
+)
+});
   app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -8,4 +20,5 @@ const app = express()
 
 app.listen(3000, () => {
   console.log(`Server is running on port 3000`);
+  
 });
